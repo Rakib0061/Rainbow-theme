2a48672c76f86e83eea890997b9e653cbf0da35c
Rakib0061 <rakibulislam0061@gmail.com>
2024-02-11 03:11:08 +0600

    done sliderpart

--- js/main.js
+++ js/main.js
@@ -20,12 +20,52 @@ const tooltipList = [...tooltipTriggerList].map(
 //  ✅@@@@@@@@@@@@@@@@@@@@@ All Swiper slider JS here @@@@@@@@@@@@@@@@@@@@@
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
   freeMode: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    // 320px এর উপরে কাজ করবে... 
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // 480px এর উপরে কাজ করবে...
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    // 640px এর উপরে কাজ করবে...
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // 740px এর উপরে কাজ করবে...
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // 992px এর উপরে কাজ করবে...
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    // 1200px এর উপরে কাজ করবে...
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
@@ -66,14 +106,14 @@ function handleNavMenuToggler(params) {
 }
 // END MENU BUTTON COLLAPS AFTER CLICK NAVLINK
 
-
 // Begin CARD BTN COLLAPSE
-const cardBtnEye = document.getElementById("cardBtnEye");
+const cardBtnEye = document.querySelectorAll(".cardBtnEye");
 const fa_eye_slash = document.querySelector(".fa-eye-slash");
 const fa_eye = document.querySelector(".fa-eye");
 
-cardBtnEye.addEventListener("click", () => {
+cardBtnEye.forEach(vlu => vlu.addEventListener("click", () => {
   fa_eye_slash.classList.toggle("d-none");
   fa_eye.classList.toggle("d-none");
-});
+}));
+
 // END CARD BTN COLLAPSE
